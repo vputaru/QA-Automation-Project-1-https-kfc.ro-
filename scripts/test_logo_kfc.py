0c372c690dbbe23a4b1fc22489d6bdff20186e91
Vlad Putaru <putaru.vlad@example.com>
2025-07-30 18:25:09 +0300

    Commit: added test function to logo test case

--- scripts/test_logo_kfc.py
+++ scripts/test_logo_kfc.py
@@ -6,11 +6,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-service = Service()
-driver = webdriver.Chrome(service=service)
-driver.get("https://kfc.ro")
+def test_kfc_logo_visible():
+    service = Service()
+    driver = webdriver.Chrome(service=service)
+    driver.get("https://kfc.ro")
 
-logo = driver.find_element(By.XPATH, '//img[@alt="kfc-romania-logo"]')
-assert logo.is_displayed(), "Logo is not visible on the homepage"
+    logo = driver.find_element(By.XPATH, '//img[@alt="kfc-romania-logo"]')
+    assert logo.is_displayed(), "Logo is not visible on the homepage"
 
-driver.quit()
+    driver.quit()
